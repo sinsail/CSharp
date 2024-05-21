9d515b5242278454eb73333abce9f435abb08b6f
Sunil Shah <sunilshah@Sunils-MacBook-Pro.local>
2024-05-21 20:28:30 +0545

    Hello

---
+++ 100_Beginner_Questions/C#_Beginner Questions.cs
@@ -0,0 +1,9 @@
+﻿// 1. Write a C# Sharp program to print Hello and your name in a separate line.
+string myName = "Sunil Shah";
+Console.WriteLine("HellO " + myName);
+
+// 2. Write a C# Sharp program to print the sum of two numbers.
+int a = 90 ;
+int b = 10;
+Console.WriteLine(a + b);
+
